9bd69c0b9df84f817c2b705a5a7b0a6505ca52f9
MedvednikovaNastya <34795089+MedvednikovaNastya@users.noreply.github.com>
2020-04-16 12:37:35 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+#include <vector>
+#include <cmath>
+using namespace std;
+int main()
+{
+    long long n,s,e,emax,summ=0,summmax=0;
+    cin >> n;
+    vector <int> a(100000000);
+    for (int i=0;i<n;i++){
+        cin >> s >> e;
+        a[s]+=1;
+        a[e]-=1;
+    }
