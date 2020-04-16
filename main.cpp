c62aa9abe3127beaa99c6543e7b16d2cf173f815
MedvednikovaNastya <34795089+MedvednikovaNastya@users.noreply.github.com>
2020-04-16 11:34:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,17 +1,1 @@
-#include <iostream> 
-using namespace std;
-int main()
-{
-    int A[] = { 500, 200, 100, 50, 20, 10 };
-    int n, a, x;
-    cin >> n ; 
-    x=0;
-    for(int i = 0; i < 6; ++i){
-        a = A[i]; 
-        x += n / a;
-        n %= a;
-    }
-    if(n > 0) x = -1;
-        cout << x; 
- return 0;
-}
+
